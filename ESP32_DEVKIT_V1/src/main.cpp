5327887936f908beacc06f50b59eefc492e4ff9f
iam7t9 <iam7t9@proton.me>
2023-09-23 19:19:01 +0530

    ESP32 Devkit OTA Setup

--- ESP32_DEVKIT_V1/src/main.cpp
+++ ESP32_DEVKIT_V1/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <Config.h>
+#include <Credentials.h>
+#include <OTA.h>
+#include <MPU6050_tockn.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+MPU6050 mpu6050(Wire);
+
+void MPU_Update( void * parameter ) {
+  for (;;) mpu6050.update();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Booting");
+  setupOTA("Line Follower", WIFI_SSID, WIFI_PASS);
+  Wire.begin();
+  mpu6050.begin();
+
+  // Config.setup()
+  pinMode(14, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(2, OUTPUT);
+  digitalWrite(27, LOW);
+
+  mpu6050.calcGyroOffsets(true);
+
+  xTaskCreate(
+    MPU_Update,          /* Task function. */
+    "MPU_Update",        /* String with name of task. */
+    10000,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
