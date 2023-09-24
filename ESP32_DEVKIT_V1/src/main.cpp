96bb337726bf34a651c558eea94e60ebb978aee5
iam7t9 <iam7t9@proton.me>
2023-09-24 13:46:15 +0530

    Minor changes

--- ESP32_DEVKIT_V1/src/main.cpp
+++ ESP32_DEVKIT_V1/src/main.cpp
@@ -1,15 +1,48 @@
 #include <Arduino.h>
-#include <Config.h>
+#include <Config.h>  // See config file for all pin maps
 #include <Credentials.h>
-#include <OTA.h>
+#include <OTA.h>  // optimization needs here
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 
+// #include <IR.h> Not Created yet
+
+// #include <HCSR04.h> Need some changes to work with RTOS updates
+// https://github.com/gamegine/HCSR04-ultrasonic-sensor-lib
+
+
 MPU6050 mpu6050(Wire);
 
+// IR IR1(pin);
+// IR IR2(pin);
+// IR IR3(pin);
+// ...
+
+// HCSR04 HC_FRONT(5, 6);
+// HCSR04 HC_LEFT(5, 6);
+// HCSR04 HC_RIGHT(5, 6);
+
+
 void MPU_Update( void * parameter ) {
-  for (;;) mpu6050.update();
+  for (;;) {
+    mpu6050.update();
+  }
 }
+
+void IR_Update( void * parameter ) {
+  for (;;) {
+    // IR1.update();
+    // ...
+  }
+}
+
+void HCSR04_Update( void * parameter ) {
+  for (;;) {
+    // HC_FRONT.update();
+    // ...
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -23,6 +56,7 @@ void setup() {
   pinMode(27, OUTPUT);
   pinMode(2, OUTPUT);
   digitalWrite(27, LOW);
+  // ^^^^^^^^^^^^^^^^^^^^^
 
   mpu6050.calcGyroOffsets(true);
 
@@ -33,6 +67,23 @@ void setup() {
     NULL,             /* Parameter passed as input of the task */
     1,                /* Priority of the task. */
     NULL);            /* Task handle. */
+    
+  xTaskCreate(
+    IR_Update,          /* Task function. */
+    "IR_Update",        /* String with name of task. */
+    1000,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
+    
+  xTaskCreate(
+    HCSR04_Update,          /* Task function. */
+    "HCSR04_Update",        /* String with name of task. */
+    1000,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
+
 }
 
 
