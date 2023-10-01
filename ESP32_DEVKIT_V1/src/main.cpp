0e1d54ae96069c0aafbc7de7ade359514bbf1f06
iam7t9 <iam7t9@proton.me>
2023-10-01 21:22:56 +0530

    Ceaning

--- ESP32_DEVKIT_V1/src/main.cpp
+++ ESP32_DEVKIT_V1/src/main.cpp
@@ -1,106 +1,108 @@
 #include <Arduino.h>
-#include <Config.h>  // See config file for all pin maps
-#include <Credentials.h>
+#include "Config.h"  // See config file for all pin maps
 #include <OTA.h>  // optimization needs here
 #include <MPU6050_tockn.h>
-#include <Wire.h>
+#include <Motor.h>
 #include <IR.h>
-#include <log.h>
 #include <HCSR04.h> // Need some changes to work with RTOS updates
-// https://github.com/gamegine/HCSR04-ultrasonic-sensor-lib
+#include <TelnetStream.h>
+#include <WebSocketsServer.h>
 
-
+WebSocketsServer webSocket = WebSocketsServer(80);
 MPU6050 mpu6050(Wire);
-
-// IR IR1(IR_1);
-// ...
-
 HCSR04 HC_FRONT(ULTRA_COMMON_TRIG, ULTRA_FRONT);
 HCSR04 HC_LEFT(ULTRA_COMMON_TRIG, ULTRA_LEFT);
 HCSR04 HC_RIGHT(ULTRA_COMMON_TRIG, ULTRA_RIGHT);
+Motor left(LEFT_F, LEFT_R);
+Motor right(RIGHT_F, RIGHT_R);
 
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
+void updateSensorsVar( void * parameter );
+void spawnTasks();
 
-void MPU_Update( void * parameter ) {
-  for (;;) {
-    mpu6050.update();
-    TelnetStream.println("MPU_UPDATE");
-    // TelnetStream.print(mpu6050.getAngleZ);
-    vTaskDelay(10/portTICK_PERIOD_MS);
-  }
-}
+void setup()
+{
+  #ifdef DEBUG
+    Serial.begin(115200);
+    TelnetStream.begin();
+  #endif
 
-void IR_Update( void * parameter ) {
-  for (;;) {
-    // TelnetStream.println("IR UPDATE");
-    vTaskDelay(10/portTICK_PERIOD_MS);
-
-    // IR1.update();
-    // ...
-  }
-}
-
-void HCSR04_Update( void * parameter ) {
-  for (;;) {
-    TelnetStream.print("HCSE04 Update: ");
-    TelnetStream.print("\t");
-    TelnetStream.print(HC_FRONT.dist());
-    TelnetStream.print(" \t");
-    TelnetStream.print(HC_LEFT.dist());
-    TelnetStream.print(" \t");
-    TelnetStream.println(HC_RIGHT.dist());
-    vTaskDelay(10/portTICK_PERIOD_MS);
-    // HC_FRONT.update();
-    // ...
-  }
-}
-
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
+  Sprintln("Booting");
   setupOTA("Line Follower", WIFI_SSID, WIFI_PASS);
+
   Wire.begin();
   mpu6050.begin();
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+  mpu6050.calcGyroOffsets();
 
-  pinMode(LEFT_F, OUTPUT);
-  pinMode(LEFT_R, OUTPUT);
-  pinMode(2, OUTPUT);
+  #if defined(ESP32_RTOS) && defined(ESP32)
+    spawnTasks();
+  #endif        
+}
 
-  delay(3000);
 
-  // analogWrite(RIGHT_F, 90);
-  // analogWrite(LEFT_F, 90);
+void updateSensorsVar( void * parameter ) {
+  for (;;) {
+    mpu6050.update();
+    webSocket.loop();
+    vTaskDelay(5/portTICK_PERIOD_MS);
+  }
+}
 
-  mpu6050.calcGyroOffsets(true);
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
 
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connection from ", num);
+        Serial.println(ip.toString());
+      }
+      break;
+
+    case WStype_TEXT:
+      {
+        TelnetStream.printf("[%u] Text: %s\n", num, payload);
+        webSocket.sendTXT(num, payload);
+      }
+      break;
+
+    case WStype_BIN:
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+
+    default:
+      break;
+  }
+}
+
+void spawnTasks()
+{
   xTaskCreatePinnedToCore(
-    MPU_Update,          /* Task function. */
-    "MPU_Update",        /* String with name of task. */
+    updateSensorsVar,    /* Task function. */
+    "MPU_Update",     /* String with name of task. */
     10000,            /* Stack size in bytes. */
     NULL,             /* Parameter passed as input of the task */
     1,                /* Priority of the task. */
     NULL,             /* Task handle. */
-    1);               
-    
+    1);               /* CPU Core */
+
   xTaskCreatePinnedToCore(
-    IR_Update,          /* Task function. */
-    "IR_Update",        /* String with name of task. */
+    ota_handle,       /* Task function. */
+    "OTA_HANDLE",     /* String with name of task. */
     10000,            /* Stack size in bytes. */
     NULL,             /* Parameter passed as input of the task */
     1,                /* Priority of the task. */
-    NULL,            /* Task handle. */
-    1);
-
-  xTaskCreatePinnedToCore(
-    HCSR04_Update,          /* Task function. */
-    "HCSR04_Update",        /* String with name of task. */
-    10000,            /* Stack size in bytes. */
-    NULL,             /* Parameter passed as input of the task */
-    2,                /* Priority of the task. */
-    NULL,            /* Task handle. */
-    1);
+    NULL,             /* Task handle. */
+    1);               /* CPU Core */
 }
-
 
 void loop() {
 }
